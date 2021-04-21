bfdfaf842d0fd007839e2a5f79c6e8b63fef4319
oiwn <alex@imscraping.ninja>
2021-04-21 10:50:25 +0700

    add docker installation

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -35,6 +35,9 @@ cd ~
 # sudo apt-get install redis
 
 # install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # install zsh & oh-my-zsh
 sudo apt-get -y install zsh
