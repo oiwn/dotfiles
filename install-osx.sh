10bd1af6ff555495880f1ef17838a5ce292c1383
oiwn <alex@imscraping.ninja>
2024-01-23 22:37:32 +0700

    remove old yaml alacritty config

--- install-osx.sh
+++ install-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # constants
-MINICONDA_FILE="Miniconda3-py39_4.9.2-Linux-x86_64.sh"
+MINICONDA_FILE="Miniconda3-py311_23.11.0-2-MacOSX-x86_64.sh"
 
 # create folders
 cd ~ && mkdir code && mkdir code/tmp
@@ -10,35 +10,25 @@ cd ~ && mkdir code && mkdir code/tmp
 cd ~/code/tmp && git clone git clone https://github.com/powerline/fonts
 cd ~/code/tmp/fonts && ./install.sh
 
-# install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install necessary utils
-brew install fzf exa htop nmap mosh tmux ripgrep fd midnight-commander git
+brew install htop nmap mosh tmux ripgrep fd midnight-commander git
 # required for python library shapely
-brew install geos
-# ctags
-brew install universal-ctags
+brew install geos universal-ctags
 
 # install mongodb
 brew tap mongodb/brew
 brew install mongodb-community
 
-# install redis
-brew install redis
-
 # docker
 brew install docker
 
-# install neovim and plug
+# install neovim
 brew install neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install software
-brew install --cask iterm2
+brew install --cask warp
 brew install --cask dash
 brew install --cask cocoarestclient
--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -9,30 +9,8 @@ sudo apt-get upgrade -y
 sudo apt-get install -y gnupg apt-transport-https ca-certificates curl lsb-release libjemalloc2 libjemalloc-dev gcc make
 
 # ================================
-# install mongodb & redis clients
+# install docker
 # ================================
-
-# mongodb
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-sudo touch /etc/apt/sources.list.d/mongodb-org-4.4.list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org-shell
-
-# redis
-cd /tmp
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-sudo cp src/redis-cli /usr/local/bin/
-sudo chmod 755 /usr/local/bin/redis-cli
-cd ~
-
-# actually can do like this
-# sudo add-apt-repository ppa:redislabs/redis
-# sudo apt-get update
-# sudo apt-get install redis
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -62,7 +40,7 @@ cd dotfiles
 make install
 
 # install conda
-MINICONDA_FILE="Miniconda3-py39_4.9.2-Linux-x86_64.sh"
+MINICONDA_FILE="Miniconda3-py311_23.11.0-2-MacOSX-x86_64.sh"
 MINICONDA_DIR="~/miniconda3"
 
 cd ~
